0f955557ba32a78a2c9ee2eb594af30e97154086
shuvo1710 <mdshovo102002@gmail.com>
2025-02-18 13:34:37 +0600

    test push

---
+++ demo/service/ProductService.tsx
@@ -0,0 +1,16 @@
+import { Demo } from '@/types';
+
+export const ProductService = {
+    getProductsSmall() {
+        return fetch('/demo/data/products-small.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then((res) => res.json())
+            .then((d) => d.data as Demo.Product[]);
+    },
+
+    getProducts() {
+        return fetch('/demo/data/products.json', { headers: { 'Cache-Control': 'no-cache' } })
+            .then((res) => res.json())
+            .then((d) => d.data as Demo.Product[]);
+    },
+
+};
---
+++ layout/AppMenu.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @next/next/no-img-element */
+
+import React, { useContext } from 'react';
+import AppMenuitem from './AppMenuitem';
+import { LayoutContext } from './context/layoutcontext';
+import { MenuProvider } from './context/menucontext';
+import Link from 'next/link';
+import { AppMenuItem } from '@/types';
+
+const AppMenu = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+
+    const model: AppMenuItem[] = [
+        {
+            label: 'Home',
+            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        {
+            label: 'Pages',
+            icon: 'pi pi-fw pi-briefcase',
+            to: '/pages',
+            items: [
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                },
+                {
+                    label: 'Empty',
+                    icon: 'pi pi-fw pi-circle-off',
+                    to: '/pages/empty'
+                }
+            ]
+        }
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
