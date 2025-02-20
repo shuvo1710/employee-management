15e4a6ae40f0d2217771c2ae9140d74b5315d957
shuvo1710 <mdshovo102002@gmail.com>
2025-02-20 08:05:46 +0600

    menu update

--- layout/AppMenuitem.tsx
+++ layout/AppMenuitem.tsx
@@ -58,12 +58,12 @@ const AppMenuitem = (props: AppMenuItemProps) => {
         <li className={classNames({ 'layout-root-menuitem': props.root, 'active-menuitem': active })}>
             {props.root && item!.visible !== false && <div className="layout-menuitem-root-text">{item!.label}</div>}
             {(!item!.to || item!.items) && item!.visible !== false ? (
-                <Link href={`${item!.url}`} onClick={(e) => itemClick(e)} className={classNames(item!.class, 'p-ripple')} target={item!.target} tabIndex={0}>
+                <a href={item!.url} onClick={(e) => itemClick(e)} className={classNames(item!.class, 'p-ripple')} target={item!.target} tabIndex={0}>
                     <i className={classNames('layout-menuitem-icon', item!.icon)}></i>
                     <span className="layout-menuitem-text">{item!.label}</span>
                     {item!.items && <i className="pi pi-fw pi-angle-down layout-submenu-toggler"></i>}
                     <Ripple />
-                </Link>
+                </a>
             ) : null}
 
             {item!.to && !item!.items && item!.visible !== false ? (
